f1595d2d3b55cb2e4ab0d0f5124e8f4028c5b18b
pasupunootinikitha <137151070+pasupunootinikitha@users.noreply.github.com>
2025-03-28 22:10:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,75 @@
+//importing all required exterenal modules after installation
+const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
+const User=require('./models/User')
+const bcrypt=require('bcryptjs')
+
+//MiddleWare
+const PORT=3000
+const app=express()
+app.use(express.json())
+
+//connecting Mongodb Atlas
+mongoose.connect(process.env.MONGO_URL).then(
+    ()=>console.log("DB connected Successfully..")
+).catch(
+    (err)=>console.log(err)
+)
+
+//API landing page http://localhost:3000/
+
+app.get('/',async(req, res)=>{
+    try{
+         res.send("<h1 align=center>welcome to the backend and week 2</h1>")
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+
+//API Registration page http://localhost:3000/register
+
+app.post('/register',async(req,res)=>{
+    const {user,email,password}= req.body
+    try{
+       const hashPassword=await bcrypt.hash(password,10)
+       const newUser=new User({user,email,password:hashPassword})
+       await newUser.save()
+       console.log("New user is registered successfully...")
+       res.json({message:'User created...'})
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+
+})
+
+//API Login page 
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body
+    try{
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) 
+            {
+             return res.status(400).json({ message: "Invalid Credentials" });
+            }
+          res.json({ message: "Login Successful", username: user.username });
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+
+
+//Server running and testing
+app.listen(PORT,(err)=>{
+
+    if(err){
+        console.log(err)
+    }
+    console.log("Server is running on port | This Niki : "+PORT)
+})
